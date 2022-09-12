349037f7c5899906fdfefd5d348e550084a84054
Alex <northern12@ya.ru>
2022-09-13 01:00:57 +0700

    Initial Commit

---
+++ HelloWorldTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldTest
+//
+//  Created by Alex Friend on 13.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
